711fcd408dcb72f6fab0d672afa18b3f77bfd3b7
Anika Christopher <anika.mchristopher@gmail.com>
2019-09-29 15:33:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
- names = ("man" => "Steve", "friend" => "Trzvi") 
+ names = ("man" => "Steve", "friend" => "Tzvi") 
+ names ["man"]
 end
 
 def update_counting_hash(hash, key)
